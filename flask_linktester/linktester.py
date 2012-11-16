179a211b8eb265e715c8f174413dc0f519487bf0
Stefane Fermigier <sf@fermigier.com>
2012-11-16 11:07:15 +0100

    More robust handling of relative urls.

--- flask_linktester/linktester.py
+++ flask_linktester/linktester.py
@@ -35,6 +35,7 @@ class LinkTester(object):
 
   def __init__(self, client, root):
     self.client = client
+    self.link_map = {}
     self.to_visit = set([root])
     self.black_list = self._black_list.copy()
     self.visited = set()
@@ -53,17 +54,17 @@ class LinkTester(object):
 
   def crawl(self):
     while self.to_visit:
-      link = self.to_visit.pop()
-      if not link or link in self.visited:
+      url = self.to_visit.pop()
+      if not url or url in self.visited:
         continue
 
       if self.verbosity >= 1:
-        print "Crawling URL:", link
+        print "Crawling URL:", url
 
       try:
-        response = self.client.get(link)
+        response = self.client.get(url)
       except:
-        print "Error fetching URL:", link
+        print "Error fetching URL:", url
         raise
 
       if self.verbosity >= 2:
@@ -71,7 +72,7 @@ class LinkTester(object):
 
       status_code = response.status_code
       assert status_code in self.allowed_codes,\
-        "Response from URL %s was %s" % (link, response.status_code)
+        "Response from URL %s was %s" % (url, response.status_code)
 
       if self.verbosity >= 2:
         print "  Got content-type:", response.content_type
@@ -80,7 +81,7 @@ class LinkTester(object):
       if not response.content_type.startswith('text/html'):
         continue
 
-      self.visited.add(link)
+      self.visited.add(url)
       parser = LinkExtractor()
       parser.feed(response.data)
 
@@ -89,8 +90,12 @@ class LinkTester(object):
 
       for new_link in parser.links:
         if not new_link.startswith("/"):
-          new_link = link.split("?")[0] + new_link
+          new_link = urlparse.urljoin(url, new_link)
         if self.blacklisted(new_link):
           continue
         if new_link not in self.visited:
-          self.to_visit.add(new_link)
+          self.add_link(url, new_link)
+
+  def add_link(self, current_url, link):
+    self.to_visit.add(link)
+    self.link_map.setdefault(current_url, set()).add(link)
